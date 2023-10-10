0ff9e5fcc0fa4999897a7ad225641738c294a91a
Jacob Soto <jacobleesoto@gmail.com>
2023-10-10 17:52:17 -0500

    Change of Plans

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React from "react";
-import "./App.css";
-import { Container, Navbar, NavbarBrand } from "reactstrap";
-import Logo from "./app/assets/images/c-logo.jpg";
+import React from 'react';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary" sticky="top" expand="md">
-        <Container>
-          <NavbarBrand href="/">
-            <img src={Logo} alt="Comedy Club logo" />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      The Comedy Club Experience!
-    </div>
-  );
+   return <div className='App'>React Project 001</div>;
 }
 
 export default App;
