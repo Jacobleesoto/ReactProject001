8d67d98e849267616d0483163b6dbd836eab5704
Jacob Soto <jacobleesoto@gmail.com>
2023-10-09 20:35:45 -0500

    header and logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import Logo from "./app/assets/images/c-logo.jpg";
 
 function App() {
-  return <div className="App">hello nucamp!</div>;
+  return (
+    <div className="App">
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={Logo} alt="Comedy Club logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      The Comedy Club Experience!
+    </div>
+  );
 }
 
 export default App;
