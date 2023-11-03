01395035bc7de1a7f580acb67940873e407e0547
Jacob Soto <jacobleesoto@gmail.com>
2023-11-02 20:13:03 -0500

    conditionally rendered dropdowns

--- src/app/shared/CONTRACTORS.js
+++ src/app/shared/CONTRACTORS.js
@@ -2,18 +2,66 @@ export const CONTRACTORS = [
    {
       id: 0,
       name: 'Empower',
+      jobs: [
+         {
+            id: 0,
+            jobName: 'Landmark',
+            jobType: 'Lighting',
+         },
+         {
+            id: 1,
+            jobName: 'Redbull',
+            jobType: 'Lighting',
+         },
+         {
+            id: 2,
+            jobName: 'Omnia 2nd Floor',
+            jobType: 'Gear',
+         },
+      ],
    },
    {
       id: 1,
       name: 'Enterprise',
+      jobs: [
+         {
+            id: 0,
+            jobName: 'First College of Medicine',
+            jobType: 'Gear',
+         },
+         {
+            id: 1,
+            jobName: 'Westover',
+            jobType: 'Pipe',
+         },
+      ],
    },
    {
       id: 2,
       name: 'Team',
+      jobs: [
+         {
+            id: 0,
+            jobName: 'Zoo Temp Parking',
+            jobType: 'Lighting',
+         },
+      ],
    },
    {
       id: 3,
       name: 'Gaylor',
+      jobs: [
+         {
+            id: 0,
+            jobName: 'Nashville Yards Tower 4',
+            jobType: 'Wire',
+         },
+         {
+            id: 1,
+            jobName: 'Tanger',
+            jobType: 'Lighting',
+         },
+      ],
    },
    {
       id: 4,
--- src/pages/AddPage.js
+++ src/pages/AddPage.js
@@ -1,13 +1,13 @@
 import { Container } from 'reactstrap';
 import BackHomeButton from '../components/BackHomeButton';
-import AddByButtonGroup from '../features/adding/AddByButtonGroup';
+import ContractorDropdown from '../features/adding/ContractorDropdown';
 
 const AddPage = () => {
    return (
       <Container>
          <BackHomeButton />
-         <h1>Add by:</h1>
-         <AddByButtonGroup />
+         <h1>Add Pallet:</h1>
+         <ContractorDropdown />
       </Container>
    );
 };
